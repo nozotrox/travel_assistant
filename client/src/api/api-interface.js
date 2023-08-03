9a5f1f8fc01f169acdd50b2798ffc42908712faf
nozotrox <fifasmazoio@gmail.com>
2023-08-04 00:04:13 +0200

    refactor: remove mock data from be and include authetication token in request params

--- client/src/api/api-interface.js
+++ client/src/api/api-interface.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getDefaultRequestConfig } from '../utils/helpers';
+import { getAuthRequestConfig, getDefaultRequestConfig } from '../utils/helpers';
 
 export const searchCity = async (cityName) => {
     try {
@@ -14,7 +14,7 @@ export const searchCity = async (cityName) => {
 export const getCityInformation = async (location) => {
     try {
         const url = `${process.env.REACT_APP_API_URL}/services/travelinfo?lat=${location.latitude}&lon=${location.longitude}`;
-        const response = await axios.get(url, getDefaultRequestConfig());
+        const response = await axios.get(url, getAuthRequestConfig());
         console.log(response.data);
         return response.data;
     } catch (error) {
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,7 +1,18 @@
-exports.getDefaultRequestConfig = () => { 
+exports.getDefaultRequestConfig = () => {
     const config = {
         headers: {
             "Content-Type": "application/json"
+        }
+    }
+
+    return config;
+}
+
+exports.getAuthRequestConfig = () => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Im5vem90cm94IiwiZW1haWwiOiJub3pveEBnbWFpbC5jb20iLCJpYXQiOjE2OTExMDAxNDAsImV4cCI6MTY5MTEwMzc0MH0.g9DSbDCBlrF9ysNuw7RSUNCbU5rUZGa7GC2wPFkjUWY",
         }
     }
 
