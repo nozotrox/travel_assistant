445dc168a6066a8964d92ac5f7ce57823b5f1fb9
nozotrox <fifasmazoio@gmail.com>
2023-08-03 03:57:05 +0200

    add: styling setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import styles_main from "./styles/main.module.scss"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="app">
+      <header id={styles_main.navbar} className="d-flex justify-content-between">
+        <div>
+          icon
+        </div>
+        <div>
+         Login
+        </div>
       </header>
     </div>
   );
