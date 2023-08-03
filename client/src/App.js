de6674498de74fef750c0cfb0e6f7ebb6dbc083b
nozotrox <fifasmazoio@gmail.com>
2023-08-03 04:24:29 +0200

    add: navigation bar layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import styles_main from "./styles/main.module.scss"
+import logo from "./assets/icons/logo.png"
 
 function App() {
   return (
     <div id="app">
-      <header id={styles_main.navbar} className="d-flex justify-content-between">
-        <div>
-          icon
+      <header id={styles_main.navbar} className="d-flex justify-content-between align-items-center">
+        <div className={styles_main.logo_container}>
+          <h1>Travel Genius</h1>
+          <span className={styles_main.dot}></span>
         </div>
-        <div>
-         Login
-        </div>
+        <ul className={styles_main.nav_items_container}>
+         <li>Login</li>
+         <li className={styles_main.active_nav}>Sign Up</li>
+        </ul>
       </header>
     </div>
   );
