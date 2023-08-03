e16c4a79a273aaa2605b479c051f099c308488e1
nozotrox <fifasmazoio@gmail.com>
2023-08-03 20:44:10 +0200

    continue: develop layout structure for weather forecast

--- client/src/components/info-panel.component.jsx
+++ client/src/components/info-panel.component.jsx
@@ -1,11 +1,22 @@
 import styles_main from "../styles/main.module.scss";
 
+import MAP_ICON_SMALL from "../assets/icons/map-pin-small.svg";
+
 const InfoPanel = () => { 
 
     return (
         <div className={styles_main.cityInfoContainer}>
             {/* weather section */}
-            
+            <div className={styles_main.bannerWeather}>
+                <div className={styles_main.cityTitle}>
+                    <img src={MAP_ICON_SMALL} alt="map_icon"/>
+                    <span className="mx-2">Matola, <span className={styles_main.fadedText}>Maputo</span></span>
+                </div>
+                <div className={styles_main.cityWeatherInfo}>
+                    
+                </div>
+            </div>
+
         </div>
     )
 }
